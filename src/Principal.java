46b717f8f9e3427230302efcd8bc19c02ebf102c
Sergio Franco <sefpnerv@hotmail.com>
2013-08-13 12:21:24 -0500

    cambios

--- src/Principal.java
+++ src/Principal.java
@@ -10,23 +10,23 @@ public class Principal {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		int x1,y1,x2,y2,x3,y3;
+		double x1,y1,x2,y2,x3,y3;
 		Punto p1;
 		Punto p2;
 		Punto p3;
 		
 		System.out.print("Ingrese Coordenada X1: ");
-		x1 =sc.nextInt();
+		x1 =sc.nextDouble();
 		System.out.print("Ingrese Coordenada Y1: ");
-		y1 =sc.nextInt();
+		y1 =sc.nextDouble();
 		System.out.print("Ingrese Coordenada X2: ");
-		x2 =sc.nextInt();
+		x2 =sc.nextDouble();
 		System.out.print("Ingrese Coordenada Y2: ");
-		y2 =sc.nextInt();
+		y2 =sc.nextDouble();
 		System.out.print("Ingrese Coordenada X3: ");
-		x3 =sc.nextInt();
+		x3 =sc.nextDouble();
 		System.out.print("Ingrese Coordenada Y3: ");
-		y3 =sc.nextInt();
+		y3 =sc.nextDouble();
 		
 		p1 = new Punto(x1,y1);
 		p2 = new Punto(x2,y2);
@@ -35,7 +35,12 @@ public class Principal {
 		Triangulo triangulo = new Triangulo(p1,p2,p3);
 		
 		System.out.println(triangulo.toString());
+		
 		System.out.println(triangulo.TipoTriangulo());
+		System.out.println("Distancia P1 a P2: "+p1.HallarDistancia(p2));
+		System.out.println("Distancia P2 a P3: "+p2.HallarDistancia(p3));
+		System.out.println("Distancia P3 a P1: "+p3.HallarDistancia(p1));
+		
 	}
 
 }
